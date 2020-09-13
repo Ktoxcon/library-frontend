b4b0b23d23caed4ef3df5587c2af3b9aff466d0d
Ktoxcon <kennethtoxro@gmail.com>
2020-09-13 11:20:30 -0600

    Shared Module added to feature module, home page exported

---
+++ src/app/feature/feature.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { HomePageComponent } from './home-page/home-page.component';
+
+@NgModule({
+  declarations: [HomePageComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [HomePageComponent],
+})
+export class FeatureModule {}
