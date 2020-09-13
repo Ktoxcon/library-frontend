abd85270d2a9e6b6f583bcca461e16cd2523c2a9
Ktoxcon <kennethtoxro@gmail.com>
2020-09-13 11:41:02 -0600

    HomePage component styles fixes

--- src/app/feature/home-page/home-page.component.ts
+++ src/app/feature/home-page/home-page.component.ts
@@ -17,6 +17,22 @@ export class HomePageComponent implements OnInit {
       alt: 'Alternative Choose a book',
       content: 'Escoge dentro de los cientos de libros con los que contamos',
     },
+    {
+      title: 'Escoge un Libro',
+      subtitle: '',
+      imgUrl:
+        'https://kentland.lib.in.us/wp-content/uploads/2018/06/library-books-background-8.jpg',
+      alt: 'Alternative Choose a book',
+      content: 'Escoge dentro de los cientos de libros con los que contamos',
+    },
+    {
+      title: 'Escoge un Libro',
+      subtitle: '',
+      imgUrl:
+        'https://kentland.lib.in.us/wp-content/uploads/2018/06/library-books-background-8.jpg',
+      alt: 'Alternative Choose a book',
+      content: 'Escoge dentro de los cientos de libros con los que contamos',
+    },
   ];
 
   ngOnInit(): void {}
