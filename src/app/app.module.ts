4ddc53713995a5e4315bdc68444d5292d645ed9d
Ktoxcon <kennethtoxro@gmail.com>
2020-09-13 11:21:48 -0600

    Refactoring of app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FeatureModule } from './feature/feature.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -12,6 +12,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FeatureModule,
     SharedModule,
   ],
   providers: [],
