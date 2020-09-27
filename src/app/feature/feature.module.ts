98acf73fd09bec7ded06bbd5fa1e2aa2c7683098
Ktoxcon <kennethtoxro@gmail.com>
2020-09-26 19:46:50 -0600

    New components and modules added to feature.module

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { AdminModule } from './admin/admin.module';
 import { HomePageModule } from './home-page/home-page.module';
 
 @NgModule({
-  imports: [CommonModule, HomePageModule],
+  imports: [CommonModule, HomePageModule, AdminModule],
 })
 export class FeatureModule {}
