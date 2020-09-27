36266dcadd8a459971a6bc7ff0711b5f09703e43
Ktoxcon <kennethtoxro@gmail.com>
2020-09-26 19:43:11 -0600

    Routing updated, nested routing deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './feature/admin/admin.component';
+import { AboutComponent } from './feature/home-page/components/about/about.component';
+import { LoginComponent } from './feature/home-page/components/login/login.component';
 import { HomePageComponent } from './feature/home-page/home-page.component';
 
 const routes: Routes = [
+  { path: 'about', component: AboutComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent },
   {
     path: '',
     component: HomePageComponent,
